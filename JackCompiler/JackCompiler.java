68da0b2a354be50d635001a49adbdf676f0dae6a
Carpenter-Sam <samsolar85@gmail.com>
2026-07-29 16:58:42 +0100

    Edited comment in main

--- JackCompiler/JackCompiler.java
+++ JackCompiler/JackCompiler.java
@@ -2,14 +2,10 @@ public class JackCompiler {
 
     public static void main(String []args) {
         try {
-            JackTokeniser jack = new JackTokeniser("test-files/test.jack");
+            JackTokeniser jack = new JackTokeniser("test-files/ArrayTest/Main.jack");
 
             // WARNING: Advance still needs to be processed/outputted one more time.
             while (jack.advance()){
-                // print "<" + tokenClassification + ">"
-                // print the current token value
-                // print "</" + tokenClassification + ">"
-                // print newLine
                 System.out.println(String.format("<%s> %s <%s>", 
                     jack.getCurrentTokenType(), jack.getCurrentToken(), jack.getCurrentTokenType()));
             }
